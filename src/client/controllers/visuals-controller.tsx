12e8014ae14049269c64c73725de70aa69aefec0
The_Pr0fessor <46141975+TrendonRobinson@users.noreply.github.com>
2025-02-27 14:18:28 -0500

    Remove Unused Equipment State Subscription in VisualsController
    - Deleted unnecessary clientProducer subscription in VisualsController
    - Simplified onStart method by removing equipment change listener
    - Removed unused import for clientProducer

--- src/client/controllers/visuals-controller.tsx
+++ src/client/controllers/visuals-controller.tsx
@@ -2,7 +2,6 @@
 
 import { Controller, OnInit, OnStart } from "@flamework/core";
 import type { LogClass } from "@rbxts/rbxts-sleitnick-log";
-import { clientProducer } from "client/store";
 
 @Controller({})
 export class VisualsController implements OnInit, OnStart {
@@ -10,16 +9,5 @@ export class VisualsController implements OnInit, OnStart {
 
 	public onInit() {}
 
-	public onStart() {
-		this.ListenForEquipmentChanges();
-	}
-
-	private ListenForEquipmentChanges() {
-		clientProducer.subscribe(
-			(state) => state.equipment,
-			(equipment) => {
-				// print(equipment);
-			},
-		);
-	}
+	public onStart() {}
 }
